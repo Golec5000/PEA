0ac4d31b4ff998a1b289a431b7ff1987ef6be259
Golec5000 <91395066+Golec5000@users.noreply.github.com>
2023-10-08 13:28:25 +0200

    Add files via upload

---
+++ pea_proj_1/src/main/java/org/application/Aprpplication.java
@@ -0,0 +1,11 @@
+package org.application;
+
+public class Aprpplication {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ pea_proj_1/src/main/java/org/helpStructures/Matrix.java
@@ -0,0 +1,4 @@
+package org.helpStructures;
+
+public class Matrix {
+}
