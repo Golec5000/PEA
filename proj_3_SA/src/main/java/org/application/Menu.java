b18dc11447c3cce0d3d7bdf2e99ff7e7ae1e2ad6
Golec5000 <91395066+Golec5000@users.noreply.github.com>
2023-11-16 12:07:21 +0100

    Add files via upload

---
+++ proj_3_SA/src/main/java/org/application/Menu.java
@@ -0,0 +1,91 @@
+package org.application;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.algoritm.AlgInterface;
+import org.algoritm.SA;
+import org.io.ReadFromFile;
+
+import java.util.Scanner;
+
+@Setter
+@Getter
+public class Menu {
+
+    private ReadFromFile readFromFile;
+    private AlgInterface alg;
+
+    public Menu() {
+
+        setReadFromFile(new ReadFromFile());
+        setAlg(null);
+
+    }
+
+    private void mainMenuDisplay() {
+
+        System.out.println("1 -> Wyczytanie macierzy z pliku .txt");
+        System.out.println("2 -> Wyświelenie wczytanej macierzy");
+        System.out.println("3 -> Rozwiązanie SA");
+        System.out.println("0 -> Zakończenie programu");
+
+    }
+
+    public void mainMenu() {
+
+        int num;
+
+        do {
+
+            mainMenuDisplay();
+            num = new Scanner(System.in).nextInt();
+
+            switch (num) {
+                case 0:
+                    break;
+
+                case 1:
+
+                    System.out.println("Podaj nazwę pliku do wczytania (bez .txt)");
+                    String filename = new Scanner(System.in).nextLine();
+                    getReadFromFile().read(filename + ".txt");
+
+                    break;
+
+                case 2:
+
+                    if (getReadFromFile().getMatrix() == null) {
+                        System.out.println("Brak wczytanej macierzy");
+                        break;
+                    }
+
+                    System.out.println("Macierz z pliku");
+                    getReadFromFile().display();
+
+                    break;
+
+                case 3:
+
+                    if (getReadFromFile().getMatrix() == null) {
+                        System.out.println("Brak wczytanej macierzy");
+                        break;
+                    }
+
+                    setAlg(new SA(getReadFromFile().getMatrix(), 0));
+                    getAlg().solve();
+                    System.out.println(getAlg());
+
+                    break;
+
+                default:
+                    System.out.println("Brak takiej opcji");
+                    break;
+
+            }
+
+        } while (num != 0);
+
+    }
+
+
+}
